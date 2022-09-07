50ce040e245d32fdc80b9fe9a016ecbf01921123
MrArgon39 <mrargon39@gmail.com>
2022-09-08 09:35:13 +1000

    Edited Speed Test to work with SimHub

--- Code/IS250_Speed_Test/IS250_Speed_Test.ino
+++ Code/IS250_Speed_Test/IS250_Speed_Test.ino
@@ -1,15 +1,17 @@
 #include <TimerOne.h>
 unsigned long t;
 float f=0;// 10f = 15 speed
-int speed = 0;//1.5 speed = 1 frequency
+int speed = 20;//1.5 speed = 1 frequency
 
 void setup(){
     pinMode(11, OUTPUT);
     Serial.begin(9600);
+    Timer1.initialize(t);
+    Timer1.pwm(11, 512);
 }
 
 void loop() { //175hz = 260km/h
-    Timer1.initialize(t);
+    Timer1.setPeriod(t);
     Timer1.pwm(11, 512);
     //f=1000000/t; // calculate the frequency 
     //f = (speed - 2) * 0.6666666; // Adjust the speed such that I actually lines up
@@ -17,7 +19,7 @@ void loop() { //175hz = 260km/h
     t=1000000/f;
     Serial.println(speed);
     speed++;
-    delay(500);
+    delay(200);
 }
 
 void speedConv(){
