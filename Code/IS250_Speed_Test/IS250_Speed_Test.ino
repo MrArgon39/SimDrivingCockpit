d293ab1682d300cac7f60b8aca585289fbcef155
MrArgon39 <mrargon39@gmail.com>
2022-09-07 22:20:29 +1000

    Added test code for the Speedometer, which works

---
+++ Code/IS250_Speed_Test/IS250_Speed_Test.ino
@@ -0,0 +1,33 @@
+#include <TimerOne.h>
+unsigned long t;
+float f=0;// 10f = 15 speed
+int speed = 0;//1.5 speed = 1 frequency
+
+void setup(){
+    pinMode(11, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop() { //175hz = 260km/h
+    Timer1.initialize(t);
+    Timer1.pwm(11, 512);
+    //f=1000000/t; // calculate the frequency 
+    //f = (speed - 2) * 0.6666666; // Adjust the speed such that I actually lines up
+    speedConv();
+    t=1000000/f;
+    Serial.println(speed);
+    speed++;
+    delay(500);
+}
+
+void speedConv(){
+    if (speed <= 70){
+        f = (speed - 2) * 0.6666666;
+    }
+    else if (speed <=140){
+        f = (speed - 1) * 0.6666666;
+    }
+    else{
+        f = speed * 0.6666666;
+    }
+}
